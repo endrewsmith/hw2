ce404ee8a3a49d0ab344964b466963c06e5a9b2f
Andrew Kuznetsov <56806222+endrewsmith@users.noreply.github.com>
2024-10-08 17:09:00 +0300

    Update HomeWork.java

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -22,10 +22,64 @@ public class HomeWork extends BinaryIntSearchTree {
      * @return массив найденных максимальных значений не более чем upperBound и длиной не более count, отсортировано от большего к меньшему
      * Сигнатуру метода не меняем
      */
-    public List<Integer> findMaxDigits(int count, int upperBound) {
-        //TODO реализовать метод
-        return null;
+
+
+
+    
+    private int size;
+
+    // Для проверки метода fill добавим метод size
+    public int size() {
+        return size;
     }
 
+    @Override
+    public boolean add(int element) {
 
+        if (root == null) {
+            root = new Node(element);
+            size++;
+            return true;
+        }
+        boolean addElement = root.add(element);
+        if (addElement) {
+            size++;
+        }
+        return addElement;
+    }
+
+    // Чтобы получить все элементы дерева, добавляем метод fill, сохраняющих все элементы в List
+    public void fill(Node node, List list) {
+        if (node != null) {
+            list.add(node.value);
+            fill(node.left, list);
+            fill(node.right, list);
+        }
+    }
+
+    public List<Integer> findMaxDigits(int count, int upperBound) {
+
+        List tmpSortList = new ArrayList();
+        List newList = new ArrayList();
+        int tmpCount = 0;
+        // Заполняем tmpSortList всеми элементами дерева
+        this.fill(this.root, tmpSortList);
+        // Сортируем полученный список по убыванию
+        Collections.sort(tmpSortList, Collections.reverseOrder());
+        // Ставим условие прерывания цикла tmpCount != count, что счетчик tmpCount
+        // добавления элемента будет равен счетчику в параметрах, цикл прервется
+        for (int i = 0; tmpCount != count && i < tmpSortList.size(); i++) {
+            // Проверка элемента на верхнюю границу
+            if ((int) tmpSortList.get(i) <= upperBound) {
+                // Если проверка пройдена добавляем элемент в итоговый список
+                newList.add(tmpSortList.get(i));
+                // Увеличиваем проверочный счетчик
+                tmpCount++;
+            }
+        }
+
+        System.out.println(tmpSortList);
+        System.out.println(newList);
+        return newList;
+    }
 }
